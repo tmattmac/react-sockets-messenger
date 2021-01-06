7832070aae2c2ef5e0a1a952c318c72d4d1a714b
Trey McDeane <tmcdeane@gmail.com>
2021-01-06 15:54:21 -0500

    finished auth routes

---
+++ server/routes/auth.js
@@ -0,0 +1,53 @@
+const User = require('../models/User');
+
+const Validator = require('validatorjs');
+const jwt = require('jsonwebtoken');
+const express = require("express");
+const router = express.Router();
+
+
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.authenticate(username, password);
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.SECRET_KEY
+    );
+    res.cookie('token', token).send({ message: "New account created" });
+  } catch (err) {
+    err.status = 401;
+    next(err);
+  }
+});
+
+router.post("/register", async (req, res, next) => {
+
+  // validate incoming data
+  const rules = {
+    username: 'alpha_dash|min:3|required',
+    password: 'min:6|required',
+    email: 'email|required'
+  };
+  const validation = new Validator(req.body, rules);
+  if (validation.fails()) {
+    const err = validation.errors.all();
+    err.status = 400;
+    return next(err);
+  }
+
+  try {
+    const { username, password, email } = req.body;
+    const user = await User.create({ username, password, email });
+    const token = jwt.sign(
+      { username: user.username },
+      process.env.SECRET_KEY
+    );
+    res.cookie('token', token).send({ message: "Successfully logged in." });
+  } catch (err) {
+    err.status = 400;
+    next(err);
+  }
+})
+
+module.exports = router;
