ddffd9698ad32c2c98a2c1c01be151bc8c767e35
Trey McDeane <tmcdeane@gmail.com>
2021-01-06 19:44:40 -0500

    fixed form hook to avoid awkward data reformatting

--- client/src/hooks/useFormData.js
+++ client/src/hooks/useFormData.js
@@ -7,33 +7,35 @@ const useFormData = (fields, defaults = {}) => {
    * { value, error } as values
    */
   const initialState = Object.fromEntries(
-    fields.map(field => [field, {
-      value: String(defaults[field] || ''),
-      error: null
-    }])
+    fields.map(field => [field, String(defaults[field] || '')])
   );
 
   const clearedState = Object.fromEntries(
-    fields.map(field => [field, { value: '', error: null }])
+    fields.map(field => [field, ''])
   );
 
   const [formData, setFormData] = useState(initialState);
+  const [errors, setErrors] = useState(clearedState);
 
   const resetForm = () => setFormData(clearedState);
   const updateFormData = (e) => {
     const { name, value } = e.target;
     setFormData(formData => {
-      return { ...formData, [name]: { value, error: null } };
+      return { ...formData, [name]: value };
+    });
+    // clear errors on change
+    setErrors(errors => {
+      return { ...errors, [name]: '' };
     });
   }
 
   const addError = (field, error) => {
-    setFormData(formData => {
-      return { ...formData, [field]: { ...formData[field], error } };
+    setErrors(errors => {
+      return { ...errors, [field]: error };
     });
   }
 
-  return [formData, updateFormData, addError, resetForm];
+  return [formData, updateFormData, errors, addError, resetForm];
 }
 
 export default useFormData;
